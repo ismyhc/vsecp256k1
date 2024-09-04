eb92adf8ec8f630dc096b91d12412dbcb8ea32cc
Jacob Davis <ismyhc@gmail.com>
2024-09-04 09:12:15 -0400

    some fixes

---
+++ tests/vsecp256k1_test.v
@@ -0,0 +1,23 @@
+import vsecp256k1
+
+fn test_new() {
+
+    ctx := vsecp256k1.create_context(vsecp256k1.context_sign | vsecp256k1.context_verify)!
+    defer { ctx.destroy() }
+
+    private_key := vsecp256k1.generate_private_key()!
+    eprintln('Created private key: ${private_key.hex()}')
+
+    keypair := ctx.create_keypair(private_key)!
+    x_pubkey, _ := ctx.create_xonly_pubkey(keypair)!
+    x_pubkey_bytes := ctx.serialize_xonly_pubkey(x_pubkey)!
+    eprintln('Created x-only public key: $x_pubkey_bytes.hex()')
+
+    message := 'Hello, Schnorr!'.bytes()
+    signature := ctx.sign_schnorr(message, keypair)!
+    eprintln('Schnorr signature: ${signature.hex()}')
+
+    is_valid := ctx.verify_schnorr(signature, message, x_pubkey)
+    eprintln('Signature is valid: $is_valid')
+
+}
